1adecd4380e424dfbf317baf05c61dd355845d52
Omochice <mochice.mls.ntl@gmail.com>
2023-12-20 19:36:44 +0900

    refactor: add documentation

--- src/normalize.ts
+++ src/normalize.ts
@@ -1,3 +1,12 @@
+/**
+ * Normalize vim plugin name
+ *
+ * @param rawName Raw plugin name string
+ * @param pattern Replace pattern string
+ * @returns The normalized one
+ *
+ * Note: Default pattern is reference to https://github.com/Shougo/dein.vim/blob/5985e18b12a9d9e0ecd1747e64da2fa384abb18c/autoload/dein/parse.vim#L94
+ */
 export function normalize(
   rawName: string,
   pattern = String.raw`^(n?vim|dps|denops)[_-]|[_-]n?vim$`,
@@ -6,6 +15,12 @@ export function normalize(
   return rawName.replaceAll(replacePattern, "");
 }
 
+/**
+ * Trim tail `.vim` pattern
+ *
+ * @param rawName Raw plugin name that maybe included `.vim` pattern
+ * @returns The trimed one
+ */
 export function trimTailDotVim(rawName: string): string {
   return rawName.replace(/\.+n?vim$/, "");
 }
