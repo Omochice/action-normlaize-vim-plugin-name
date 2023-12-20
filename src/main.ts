fb815b5597c5ea67c6017bcd6a175671603fc854
Omochice <mochice.mls.ntl@gmail.com>
2023-12-20 19:29:28 +0900

    refactor: remove unneeded asyncs

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { debug, getInput, info, setOutput } from "npm:@actions/core@1.10.1";
 import { context } from "npm:@actions/github@6.0.0";
 import { normalize, trimTailDotVim } from "./normalize.ts";
 
-const main = async () => {
+const main = () => {
   info("Start main process");
   debug(JSON.stringify(context.repo));
   const stripDotVim = getInput("strip-dot-vim", { required: false }) === "true";
@@ -15,6 +15,5 @@ const main = async () => {
     "normalizedName",
     stripDotVim ? trimTailDotVim(normalized) : normalized,
   );
-  return await Promise.resolve(undefined);
 };
 main();
