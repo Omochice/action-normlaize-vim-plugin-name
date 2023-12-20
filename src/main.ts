86de03e0e2304425ff28989f403ff5e35b0d8be3
Omochice <mochice.mls.ntl@gmail.com>
2023-12-20 18:44:45 +0900

    chore: simple workable

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
-import { debug, getInput, info } from "npm:@actions/core@1.10.1";
-import { writeSummary } from "./github.ts";
+import { debug, getInput, info, setOutput } from "npm:@actions/core@1.10.1";
+import { context } from "npm:@actions/github@6.0.0";
+import { normalize, trimTailDotVim } from "./normalize.ts";
 
 const main = async () => {
   info("Start main process");
-  const message = getInput("message", { required: true });
-
-  debug(message);
-  await writeSummary("main", message);
-
+  debug(JSON.stringify(context.repo));
+  const stripDotVim = getInput("strip-dot-vim", { required: false }) === "true";
+  if (stripDotVim) {
+    info(`You specify strip-dot-vim.`);
+  }
   info("Complete main process");
+  const normalized = normalize(context.repo.repo);
+  setOutput("normalizedName", stripDotVim ? trimTailDotVim(normalized) : normalized);
+  return await Promise.resolve(undefined);
 };
 main();
